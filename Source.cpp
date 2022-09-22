b477c621e8154550897157d75ef32497943f6439
lilmoonly <114170538+lilmoonly@users.noreply.github.com>
2022-09-22 19:31:56 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,23 +1,21 @@
-#include <iostream>
-#include <string>
-#include<math.h>
+﻿#include <iostream>
+#define _USE_MATH_DEFINES 
+#include <cmath>
+#include <math.h>
 using namespace std;
+
 
 int main()
 {
-	int a;
-	int b;
-	float aref;
-	float geom;
-	cout << "Enter the number a:" << endl;
-	cin >> a;
-	cout << "Enter the number b:" << endl;
-	cin >> b;
-	aref = (a + b) / 2;
-	geom = sqrt((a * b));
-	cout << "Arithmetic average value = " << aref << endl;
-	cout << "Geometric mean value = " << geom << endl;
-
-
+	float r;
+	cout << "Enter the radius:" << endl;
+	cin >> r;
+	float l;
+	float s;
+	float v;
+	l = r * 2 * M_PI;
+	s = pow(r, 2) * M_PI;
+	v = 4 / 3 * M_PI * pow(r, 3);
+	cout << "Circle length = " << l << endl << "Circle area = " << s << endl << "Volume of the sphere = " << v << endl;
 	return 0;
 }
