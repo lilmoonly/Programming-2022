3b29fb20ca80d96cce74a410cb7da57c28f3603b
lilmoonly <114170538+lilmoonly@users.noreply.github.com>
2022-09-22 19:41:07 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#define _USE_MATH_DEFINES 
+#include <cmath>
+#include <math.h>
+using namespace std;
+
+
+int main()
+{
+	float r;
+	cout << "Enter the radius:" << endl;
+	cin >> r;
+	float l;
+	float s;
+	float v;
+	l = r * 2 * M_PI;
+	s = pow(r, 2) * M_PI;
+	v = 4 / 3 * M_PI * pow(r, 3);
+	cout << "Circle length = " << l << endl << "Circle area = " << s << endl << "Volume of the sphere = " << v << endl;
+	return 0;
+}
