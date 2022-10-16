2c491367e6da9862a2113ff95304dc23bcc3917f
lilmoonly <bohdan.muliarchyk@lnu.edu.ua>
2022-10-16 19:38:56 +0300

    Home Work and Class Work

---
+++ Home works/Home work 05.10/Task 18.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num;
+    int a;
+    int b;
+    int c;
+    cout<<"Enter the number: "<<endl;
+    cin>>num;
+    if (num >=100 and num <= 999) {
+        a = num % 10;
+        b = num % 100 / 10;
+        c = num % 1000 / 100;
+        cout << a + b + c << endl;
+    }
+    else{
+        cout<<"Incorrect number"<<endl;
+    }
+    return 0;
+}
---
+++ Home works/Home work 05.10/Task 19.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    int a;
+    int b;
+    int c;
+    cout<<"Enter the first number: "<< endl;
+    cin>> a;
+    cout<<"Enter the second number: "<< endl;
+    cin>> b;
+    cout<<"Enter the third number: "<< endl;
+    cin>> c;
+    if (0<= a and a < 24 and b >= 0 and b < 60 and c >= 0 and c < 60) {
+        cout << "Yes, you can do this!" << endl;
+    }
+    else if (0<= b and b < 24 and a >= 0 and a < 60 and c >= 0 and c < 60) {
+        cout << "Yes, you can do this!" << endl;
+    }
+    else if (0<= c and c < 24 and b >= 0 and b < 60 and a >= 0 and a < 60) {
+        cout << "Yes, you can do this!" << endl;
+    }
+    else {
+        cout<< "No, you can't do this!"<< endl;
+    }
+    return 0;
+}
